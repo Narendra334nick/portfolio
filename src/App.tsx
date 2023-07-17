44430ee64eccac89ac231fc6269c3721bcdc50dd
Narendra <narendra.singh@polestarllp.com>
2023-07-17 22:11:40 +0530

    react-router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import './App.css';
 import LandingPage from './containers/LandingPage/LandingPage';
+import ErrorPage from './containers/Core/Error';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Layout from './containers/Layout/Layout';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+    errorElement: <ErrorPage />,
+  },
+  {
+    path: "/layout",
+    element: <Layout />,
+    errorElement: <ErrorPage />,
+  },
+]);
 
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      {/* <LandingPage /> */}
+      <RouterProvider router={router} />
     </div>
   );
 }
--- src/containers/LandingPage/LandingPage.tsx
+++ src/containers/LandingPage/LandingPage.tsx
@@ -1,16 +1,21 @@
 import classes from '../LandingPage/LandingPage.module.scss';
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward';
 import { Box } from '@mui/material';
+import {useNavigate} from 'react-router-dom';
 
 export default function LandingPage() {
+  const navigate = useNavigate();
+  const viewMyWorks = () =>{
+    navigate('/layout');
+  }
   return (
     <div className={classes['container']}>
       <Box p={1}>
       <Box p={1}>Hello, I'm <span className={classes['nameContainer']}>Narendra Pratap Singh</span>.</Box>
       <Box p={1}>I'm a fullstack web-developer.</Box>
       <Box p={0.5} m={1}>
-        <span className={classes['buttonContainer']}>
-        View my works <ArrowForwardIcon/>
+        <span className={classes['buttonContainer']} onClick={()=>viewMyWorks()}>
+          View my works <ArrowForwardIcon/>
         </span>
       </Box>
       </Box>

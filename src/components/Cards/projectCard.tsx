6723cc815937aa0d2be8a96130e5b82aa415e522
Narendra <narendra.singh@polestarllp.com>
2023-09-03 16:48:37 +0530

    overlay added

--- src/components/Cards/projectCard.tsx
+++ src/components/Cards/projectCard.tsx
@@ -1,11 +1,36 @@
+import classes from "./projectCard.module.scss";
 interface ProjectCardPropType {
 	url: string;
+	name: string;
+	link?: string;
+	description?: string;
+	techStack?: string;
 }
 export default function ProjectCard(props: ProjectCardPropType) {
-	const { url } = props;
+	const { url, name, link, description, techStack } = props;
+	const handleClick = () => {
+		window.open(link, "_blank");
+	};
 	return (
-		<div>
-			<img src={url} alt="Project Img" style={{ height: 350, width: 350 }} />
+		<div className={classes["root"]}>
+			<div className={classes["imageConainer"]}>
+				<img
+					src={url}
+					alt="Project Img"
+					style={{ height: 350, width: 350 }}
+					onClick={handleClick}
+				/>
+				<div className={classes["overlay"]}>
+					<div className={classes["overlay-text"]}>
+						<div className={classes["techStack"]}>{techStack}</div>
+						<div onClick={handleClick} className={classes["learnMore"]}>
+							LEARN MORE
+						</div>
+					</div>
+				</div>
+				<div>{name && name}</div>
+				<div>{description && description}</div>
+			</div>
 		</div>
 	);
 }
--- src/containers/Portfolio/Portfolio.tsx
+++ src/containers/Portfolio/Portfolio.tsx
@@ -4,15 +4,29 @@ import { Box } from "@mui/material";
 import url from "../../assets/generalImages/projects.jpg";
 
 export default function Portfolio() {
+	const link = "https://c2c.vega-hr.com";
 	return (
 		<div>
 			Portfolio
 			<Box display={"flex"} flexGrow={1}>
+				<ProjectCard
+					url={url}
+					name={"C2C"}
+					link={link}
+					description={"description"}
+					techStack="Reactjs / Nodejs"
+				/>
+				<ProjectCard
+					url={url}
+					name={"C2C"}
+					link={link}
+					description={"description"}
+					techStack="Reactjs / Nodejs"
+				/>
+				{/* <ProjectCard url={url} />
 				<ProjectCard url={url} />
 				<ProjectCard url={url} />
-				<ProjectCard url={url} />
-				<ProjectCard url={url} />
-				<ProjectCard url={url} />
+				<ProjectCard url={url} /> */}
 			</Box>
 		</div>
 	);
